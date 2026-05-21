59f6eca6285f6d07081203525f9e4a71fce83276
cuongpedri2412 <cuongpedri077681@gmail.com>
2026-05-21 15:07:45 +0700

    api update new

--- src/main/java/org/library/manager/service/impl/BookCategoryServiceImpl.java
+++ src/main/java/org/library/manager/service/impl/BookCategoryServiceImpl.java
@@ -69,7 +69,6 @@ public class BookCategoryServiceImpl implements BookCategoryService {
 
         return toResponse(bookCategoryRepo.save(entity));
 
-
     }
     @Override
     public BookCategoryResponse deactivation(Long id, DeactivationReason reason){
