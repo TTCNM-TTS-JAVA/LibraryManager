efc093acdcd1923c7e240925def9bdb877bf6f1b
cuongpedri2412 <cuongpedri077681@gmail.com>
2026-05-21 16:18:05 +0700

    pageable

--- src/main/java/org/library/manager/repository/BookCategoryRepository.java
+++ src/main/java/org/library/manager/repository/BookCategoryRepository.java
@@ -9,8 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
     boolean existsByNameIgnoreCaseAndStatus(String name, Status status);
